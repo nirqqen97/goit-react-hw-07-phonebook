69aaf9e9d3fec6dba4581b184b904534f3cc61d1
nirqqen97 <93587073+nirqqen97@users.noreply.github.com>
2023-02-24 22:22:20 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,63 @@
-  import shortid from "shortid";
-  import { Form } from "./Form/Form";
-  import {Contacts } from "./Contacts/Contacts";
-  import {InputFilter} from "./InputFilter/InputFilter";
-  import {Container,Title} from "./App.styled";
-  import { useDispatch, useSelector } from "react-redux";
-  import { selectFilters, } from "redux/Contacts/Contacts.selector";
-  import { usersSearchAction,} from "redux/Contacts/Contacts.slice";
-  import { useGetContactsQuery,useDeleteContactsMutation } from "redux/rtk-contacts/rtk-contacts.api";
+import shortid from "shortid";
+import { Form } from "./Form/Form";
+import {Contacts } from "./Contacts/Contacts";
+import {InputFilter} from "./InputFilter/InputFilter";
+import {Container,Title} from "./App.styled";
+import { useDispatch, useSelector } from "react-redux";
+import { selectFilters, } from "redux/Contacts/Contacts.selector";
+import { usersSearchAction,} from "redux/Contacts/Contacts.slice";
+import { useGetContactsQuery, useDeleteContactsMutation, useAddUserMutation } from "redux/rtk-contacts/rtk-contacts.api";
 
 
-  export const App = () =>{
-    const {data, isLoading, isSuccess}= useGetContactsQuery()
+export const App = () =>{
+  const {data, isLoading, isSuccess}= useGetContactsQuery()
 
-    const [trigger] = useDeleteContactsMutation()
+  const [triggerDelete] = useDeleteContactsMutation()
+  const [addUser] = useAddUserMutation()
 
-    const filter = useSelector(selectFilters)
+  const filter = useSelector(selectFilters)
 
-    const dispatch = useDispatch()
-    
-    const addFilter = (value) => {
-      dispatch(usersSearchAction(value))
-    }
+  const dispatch = useDispatch()
+  
+  const addFilter = (value) => {
+    dispatch(usersSearchAction(value))
+  }
 
   const deleteFromContacts = (contactToDelete) =>{
-      // const deletedList = data.filter(contact => contact.id !== contactToDelete.id);
-      trigger(contactToDelete.id)
+    triggerDelete(contactToDelete.id)
   }
   
   const checkIsInContacts = (value) => {
     const checked = data.find(contact => contact.name === value) !== undefined;
     return checked
   }
-  
-  const addContact = (name,telephone) =>{
-    if (checkIsInContacts(name) ) {
-      alert(`${name} is already in contacts`)
-      return
+  const addContact = (name, telephone) => {
+    if (checkIsInContacts(name)) {
+      alert(`${name} is already in contacts`);
+      return;
     }
     const contact = {
-      id : shortid.generate(),
+      id: shortid.generate(),
       name,
       telephone,
-    }
-    
-  }
+    };
+    addUser(contact); 
+  };
+  
+
   const contactsFilter = () => {
-    
     const filtered = data.filter(contact =>
       contact.name.toLowerCase().trim().includes(filter.toLowerCase().trim()));
     return filtered;
   };
-    return (
-            <Container>
-              <Form onSubmit = {addContact}/>
-              <Title>Contacts</Title>
-              <InputFilter onInput = {addFilter} value = {filter}/>
-              {isLoading && <Title>Loading...</Title>}
-              {isSuccess &&  <Contacts contacts = {contactsFilter()} deleteFromContacts = {deleteFromContacts}/>}
 
-              </Container>
-
-            )
-  }
+  return (
+    <Container>
+      <Form onSubmit={addContact}/>
+      <Title>Contacts</Title>
+      <InputFilter onInput={addFilter} value={filter}/>
+      {isLoading && <Title>Loading...</Title>}
+      {isSuccess &&  <Contacts contacts={contactsFilter()} deleteFromContacts={deleteFromContacts}/>}
+    </Container>
+  )
+}
--- src/redux/rtk-contacts/rtk-contacts.api.js
+++ src/redux/rtk-contacts/rtk-contacts.api.js
@@ -3,7 +3,7 @@ import { createApi,fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const contactsApi = createApi({
     reducerPath: 'rtk-contacts',
     tagTypes: ['Contacts'],
-    baseQuery: fetchBaseQuery({baseUrl:'https://63f793b8e8a73b486afb43d3.mockapi.io/'}),
+    baseQuery: fetchBaseQuery({baseUrl:'https://63f793b8e8a73b486afb43d3.mockapi.io'}),
     providesTags: ({data}) =>
     data?
         [
@@ -16,14 +16,23 @@ export const contactsApi = createApi({
         getContacts : builder.query({
             query: () =>'/contacts'
         }),
-        deleteContacts : builder.mutation({
-            query : (id) => ({
-                url: `contacts/:${id}`,
-                method: 'DELETE',
-            })
+        deleteContacts: builder.mutation({
+            query: (id) => ({
+              url: `contacts/:${id}`,
+              method: 'DELETE',
+            }),
+          }),
+      
+          addUser: builder.mutation({
+            query: (newUser) => ({
+              url: '/contacts',
+              method: 'POST',
+              body: newUser,
+            }),
+          }),
+      
+          invalidatesTags: (result, error, id) => [{ type: 'Contacts', id }],
         }),
-        invalidatesTags: (result, error, id) => [{ type: 'Contacts', id }],
-    }),
-})
+      })
 
-export const {useGetContactsQuery,useDeleteContactsMutation} = contactsApi   
+export const {useGetContactsQuery,useDeleteContactsMutation, useAddUserMutation} = contactsApi   
