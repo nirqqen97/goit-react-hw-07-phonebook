b84cff360ac0c1cd2e8c8fa6d5a332bf7e195688
nirqqen97 <93587073+nirqqen97@users.noreply.github.com>
2023-02-25 13:35:02 +0200

    Update rtk-contacts.api.js

--- src/redux/rtk-contacts/rtk-contacts.api.js
+++ src/redux/rtk-contacts/rtk-contacts.api.js
@@ -24,7 +24,7 @@ export const contactsApi = createApi({
     }),
     addUser: builder.mutation({
       query: (newUser) => ({
-        url: '/contacts',
+        url: 'contacts',
         method: 'POST',
         body: newUser,
       }),
